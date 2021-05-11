002b60a0fad51bf79160cba8ca821bef4e489cc9
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2021-05-12 00:33:53 +0530

    Added: Database models

---
+++ app/models/__init__.py
@@ -0,0 +1,3 @@
+from .db import db
+from .message_to_forward import MessageToForward
+from .user import User
---
+++ app/models/user.py
@@ -0,0 +1,15 @@
+import datetime
+
+from app.models import db
+
+
+class User(db.Document):
+    """
+    User Model
+    """
+    username = db.StringField(unique=True, required=True, null=False, max_length=30)
+    password = db.StringField(required=True, null=False, max_length=30)
+    created_at = db.DateTimeField(null=False, default=datetime.datetime.utcnow)
+
+    def __str__(self):
+        return f'<USER: {self.pk}>'
