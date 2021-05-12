335824f3cee04ee7d5b68c96f43ef53cc326c884
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2021-05-13 03:20:25 +0530

    Updating config.py and .env.example for JWT variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
+from datetime import timedelta
+
 from flask import Flask
 from flask_bcrypt import Bcrypt
 
 from . import config
+from .authentication import jwt
 from .models import db
 
 
@@ -11,6 +14,19 @@ def create_flask_app():
     # Initializing MongoDB Connection
     flask_app.config['MONGODB_SETTINGS'] = {'host': config.MONGODB_CONNECTION_STRING}
     db.init_app(flask_app)
+
+    # Initializing JWT Manager
+    flask_app.config['JWT_SECRET_KEY'] = config.JWT_SECRET_KEY
+    flask_app.config['JWT_TOKEN_LOCATION'] = ["cookies"]
+    flask_app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=10)
+    flask_app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=1)
+    jwt.init_app(flask_app)
+
+    # CSRF
+    flask_app.config['JWT_ACCESS_CSRF_COOKIE_NAME'] = config.JWT_ACCESS_CSRF_COOKIE_NAME
+    flask_app.config['JWT_ACCESS_CSRF_HEADER_NAME'] = config.JWT_ACCESS_CSRF_HEADER_NAME
+    flask_app.config['JWT_REFRESH_CSRF_COOKIE_NAME'] = config.JWT_REFRESH_CSRF_COOKIE_NAME
+    flask_app.config['JWT_REFRESH_CSRF_HEADER_NAME'] = config.JWT_REFRESH_CSRF_HEADER_NAME
 
     # Initializing Bcrypt module
     bcrypt = Bcrypt(flask_app)
