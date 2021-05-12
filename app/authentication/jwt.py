f7c637fa9eb2779cf75c63c2725bfc5da6ff3fee
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2021-05-13 03:17:52 +0530

    Adding a JWT Handler

---
+++ app/authentication/jwt.py
@@ -0,0 +1,51 @@
+from flask import make_response
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, unset_jwt_cookies, jwt_required, \
+    unset_access_cookies
+
+from ..exceptions import InvalidTokenException, InvalidAccessTokenException
+from ..models import User
+
+jwt = JWTManager()
+
+
+class JWTHandler:
+
+    @classmethod
+    def generate_tokens(cls, user: User):
+        session_data = {"id": user.get_mongo_id(), "username": user.username}
+        access_token = create_access_token(identity=session_data, fresh=True)
+        refresh_token = create_refresh_token(identity=session_data)
+        return access_token, refresh_token
+
+    @classmethod
+    def generate_access_token(cls, identity):
+        return create_access_token(identity=identity, fresh=False)
+
+    @staticmethod
+    @jwt.unauthorized_loader
+    def unauthorized_callback(callback):
+        resp = make_response(InvalidAccessTokenException().json(), InvalidAccessTokenException.RESPONSE_CODE)
+        return resp
+
+    @staticmethod
+    @jwt.expired_token_loader
+    def expired_token_callback(callback):
+        resp = make_response(InvalidAccessTokenException().json(), InvalidAccessTokenException.RESPONSE_CODE)
+        unset_access_cookies(resp)
+        return resp
+
+    @staticmethod
+    @jwt.expired_token_loader
+    def expired_token_callback(callback):
+        resp = make_response(InvalidAccessTokenException().json(), InvalidAccessTokenException.RESPONSE_CODE)
+        unset_access_cookies(resp)
+        return resp
+
+    @staticmethod
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(callback):
+        resp = make_response(InvalidTokenException().json(), InvalidTokenException.RESPONSE_CODE)
+        unset_jwt_cookies(resp)
+        return resp
+
