ce969729761e2870af2cc276444907f38c5ca73a
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2021-05-13 02:55:23 +0530

    Updated User model to store hashed passwords

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 
 from . import config
 from .models import db
@@ -10,5 +11,8 @@ def create_flask_app():
     # Initializing MongoDB Connection
     flask_app.config['MONGODB_SETTINGS'] = {'host': config.MONGODB_CONNECTION_STRING}
     db.init_app(flask_app)
+
+    # Initializing Bcrypt module
+    bcrypt = Bcrypt(flask_app)
 
     return flask_app
--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,7 @@
 import datetime
 
+from flask_bcrypt import generate_password_hash, check_password_hash
+
 from . import db
 
 
@@ -8,8 +10,22 @@ class User(db.Document):
     User Model
     """
     username = db.StringField(unique=True, required=True, null=False, max_length=30)
-    password = db.StringField(required=True, null=False, max_length=30)
+    hashed_password = db.StringField(required=True, null=False)
     created_at = db.DateTimeField(null=False, default=datetime.datetime.utcnow)
+
+    @property
+    def password(self):
+        return self.hashed_password
+
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password) -> bool:
+        return check_password_hash(self.hashed_password, password)
+
+    def get_mongo_id(self) -> str:
+        return str(self.pk)
 
     def __str__(self):
         return f'<USER: {self.pk}>'
