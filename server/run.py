aff144f6308e39fe5be53c535f34913604aadac1
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2021-05-16 06:19:57 +0530

    Adding config to App startup

--- server/run.py
+++ server/run.py
@@ -1,6 +1,6 @@
-from app import create_flask_app, config, socket_io
+from app import create_flask_app, socket_io, config
 
 flask_app = create_flask_app()
 
 if __name__ == '__main__':
-    socket_io.run(flask_app)
+    socket_io.run(flask_app, host=config.APP_HOST, debug=config.APP_DEBUG, port=config.APP_PORT)
