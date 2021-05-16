102500872d10aff6b30bab05b964c412a882b9fb
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2021-05-16 06:17:52 +0530

    Removing jwt error handlers

--- server/app/authentication/jwt.py
+++ server/app/authentication/jwt.py
@@ -1,8 +1,5 @@
-from flask import make_response
-from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, unset_jwt_cookies, jwt_required, \
-    unset_access_cookies
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token
 
-from ..exceptions import InvalidTokenError, InvalidAccessTokenError
 from ..models import User
 
 jwt = JWTManager()
@@ -20,32 +17,3 @@ class JWTHandler:
     @classmethod
     def generate_access_token(cls, identity):
         return create_access_token(identity=identity, fresh=False)
-
-    @staticmethod
-    @jwt.unauthorized_loader
-    def unauthorized_callback(callback):
-        resp = make_response(InvalidAccessTokenException().json(), InvalidAccessTokenException.RESPONSE_CODE)
-        return resp
-
-    @staticmethod
-    @jwt.expired_token_loader
-    def expired_token_callback(callback):
-        resp = make_response(InvalidAccessTokenException().json(), InvalidAccessTokenException.RESPONSE_CODE)
-        unset_access_cookies(resp)
-        return resp
-
-    @staticmethod
-    @jwt.expired_token_loader
-    def expired_token_callback(callback):
-        resp = make_response(InvalidAccessTokenException().json(), InvalidAccessTokenException.RESPONSE_CODE)
-        unset_access_cookies(resp)
-        return resp
-
-    @staticmethod
-
-    @jwt.invalid_token_loader
-    def invalid_token_callback(callback):
-        resp = make_response(InvalidTokenException().json(), InvalidTokenException.RESPONSE_CODE)
-        unset_jwt_cookies(resp)
-        return resp
-
